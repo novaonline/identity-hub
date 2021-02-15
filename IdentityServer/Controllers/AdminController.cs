33427f67c46198041bb314683fc964fa2618e978
Emmanuel Quagraine <eman_the_man@hotmail.com>
2021-02-15 13:30:52 -0600

    Admin updated

--- IdentityServer/Controllers/AdminController.cs
+++ IdentityServer/Controllers/AdminController.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using static IdentityModel.OidcConstants;
